614fffbef000833dd59507a8527ade7962a4e6e5
genapps <genapps2016@gmail.com>
2025-06-15 07:33:57 -0300

    Otimizando o Sistema Bancário com Funções Python

--- 03 - Programação Orientada a Objetos/Modelando o Sistema Bancário em POO com Python/desafio_v1.py
+++ 03 - Programação Orientada a Objetos/Modelando o Sistema Bancário em POO com Python/desafio_v1.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractclassmethod, abstractproperty
 from datetime import datetime
 
-
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
--- 03 - Programação Orientada a Objetos/Modelando o Sistema Bancário em POO com Python/desafio_v2.py
+++ 03 - Programação Orientada a Objetos/Modelando o Sistema Bancário em POO com Python/desafio_v2.py
@@ -2,7 +2,6 @@ import textwrap
 from abc import ABC, abstractclassmethod, abstractproperty
 from datetime import datetime
 
-
 class Cliente:
     def __init__(self, endereco):
         self.endereco = endereco
